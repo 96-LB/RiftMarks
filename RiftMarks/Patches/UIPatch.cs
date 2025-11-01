1d8a6eb6f07cc32fdf8376ee9927f5351c8ca793
Lucas LaBuff <lucasbeef00@gmail.com>
2025-11-01 12:58:13 +0100

    minor bugfix with score display

---
+++ RiftMarks/Patches/UIPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RhythmRift;
+
+namespace RiftMarks.Patches;
+
+
+
+[HarmonyPatch(typeof(RRStageUIView))]
+public static class UIPatch {
+    [HarmonyPatch(nameof(RRStageUIView.UpdateScore))]
+    [HarmonyPostfix]
+    public static void UpdateScore(RRStageUIView __instance) {
+        __instance._scoreText.overflowMode = TMPro.TextOverflowModes.Overflow;
+        __instance._scoreText.enableWordWrapping = false;
+    }
+}
